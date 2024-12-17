83f8dc198ff2b5a6f83861e475ca8ac2af2f6aa2
Phạm Ngọc Huy Hùng <hungpnh96@gmail.com>
2024-12-17 23:21:58 +0700

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ particlesJS("snowfall", {
     shape: {
       type: "image",
       image: {
-        src: "image/pngegg.png", // Đường dẫn hình bông tuyết
+        src: "image/snowflake.png", // Đường dẫn hình bông tuyết
         width: 20,
         height: 20
       }
@@ -20,13 +20,17 @@ particlesJS("snowfall", {
 });
 
 
+
+
 // Hiển thị thiệp và ẩn nút
 function showCard() {
   const card = document.getElementById("card");
   const button = document.getElementById("show-wish-btn");
+  const treeContainer = document.getElementById("tree-container");
   card.classList.remove("hidden");
   card.classList.add("animate__fadeIn");
   button.style.display = "none"; // Ẩn nút
+  treeContainer.style.display = "none"; // Ẩn cây thông
 }
 
 // Đóng thiệp
@@ -34,7 +38,10 @@ function closeCard() {
   const card = document.getElementById("card");
   card.classList.add("hidden"); // Ẩn thiệp khi đóng
   const button = document.getElementById("show-wish-btn");
+  const treeContainer = document.getElementById("tree-container");
   button.style.display = "inline-block"; // Hiển thị lại nút chúc mừng
+  treeContainer.style.display = "block"; // Hiển thị lại cây thông
+
 }
 
 // Phát hoặc dừng nhạc
@@ -53,4 +60,24 @@ function toggleMusic() {
   isPlaying = !isPlaying;
 }
 
+function generateTrees() {
+  const treeContainer = document.getElementById('tree-container');
+  treeContainer.innerHTML = ''; // Xóa cây cũ nếu có
+  
+  const screenWidth = window.innerWidth - 70; // Chừa 150px bên phải
+  const treeWidth = 80;
+  const spacing = 0;
+  const totalSlots = Math.floor(screenWidth / (treeWidth + spacing));
+
+  for (let i = 0; i < totalSlots; i++) {
+    const leftPosition = i * (treeWidth + spacing);
+
+    const tree = document.createElement('div');
+    tree.classList.add('tree');
+    tree.style.left = `${leftPosition}px`;
+    treeContainer.appendChild(tree);
+  }
+}
+
+window.onload = generateTrees;
 
